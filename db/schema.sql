c162d5ecb7710a0424aed25cf937743efff7795e
JGSims <josi3006@hotmail.com>
2020-02-12 17:23:44 -0500

    Populated database using seed sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,14 @@
+DROP DATABASE burgers_db;
+
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT,
-    burger_name STRING(30),
+    burger_name VARCHAR(30),
     devoured BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY(id)
 );
