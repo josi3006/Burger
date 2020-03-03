64a8b39f582b4202aa171d171064e82f8b143083
JGSims <josi3006@hotmail.com>
2020-03-02 21:32:02 -0500

    Fixing Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 var mysql = require("mysql");
-
+var connection;
 // This code sets up the connection
 
-var connection = mysql.createConnection({
+
+if (process.env.JAWSDB_URL) {
+
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  // port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
+
+};
 
 
 // This code starts the connection
--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
 
 
-const PORT = process.env.PORT || 8080;
 
-const app = express();
 
 // Established default directory (where index is located)
 
-app.use(express.static("public"));
+
 
 
 // Parse application body as JSON
 
+const PORT = process.env.PORT || 8080;
+
+
+
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 
 // Require handlebars
 
+
+
 var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -31,6 +37,7 @@ const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Spin up the server
+
 
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
